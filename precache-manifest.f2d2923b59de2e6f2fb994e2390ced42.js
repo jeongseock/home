31d9bd79168c998688ed9a1d54fd6cf806bc3883
Jeong Lee <jsl133@ucsd.edu>
2019-09-03 06:41:52 -0700

    Updates

---
+++ precache-manifest.f2d2923b59de2e6f2fb994e2390ced42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888d6aaa197953e67185cbd639232edd",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "a5cde60363dca1b6c291",
+    "url": "/home/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "82d8cd5e79956175c8bf",
+    "url": "/home/static/js/2.eae62fc2.chunk.js"
+  },
+  {
+    "revision": "a5cde60363dca1b6c291",
+    "url": "/home/static/js/main.b58bc70f.chunk.js"
+  },
+  {
+    "revision": "9e2f01b3ff9dbff1191c",
+    "url": "/home/static/js/runtime~main.eb6df2a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home/static/media/logo.5d5d9eef.svg"
+  }
+]);
